320fcf8cfaef0b7b631e4bc742c0426a83bd3586
res Arfita <hresaarfita@gmail.com>
2025-05-28 10:32:51 +0700

    add items on Books.js

--- src/data/Books.js
+++ src/data/Books.js
@@ -4,6 +4,9 @@ const books = [
     title: "My Hero, My Boss",
     cover:
       "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+     latestUpdate: "2 new parts",
+    lastRead: "part 16",
+    author: "reffect",
     views: "1.33M",
     votes: "14.8K",
     tags: ["dark", "cinta", "adult", "+15 more"],
@@ -41,40 +44,160 @@ const books = [
     title: "The Antagonist",
     cover:
       "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    latestUpdate: "3 new parts",
+    lastRead: "part 2",
+    author: "author name 2",
     views: "7.65M",
     votes: "408K",
-    chapters: "74",
     tags: ["miskin", "fiksi", "hidupkembali", "+22 more"],
+    chapters: "74",
+    chapterContent: [
+      {
+        id: 1,
+        subtitle: "Chapter 1",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 2,
+        subtitle: "Chapter 2",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 3,
+        subtitle: "Chapter 3",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 4,
+        subtitle: "Chapter 4",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 5,
+        subtitle: "Chapter 5",
+        content: "lorem ipsum....."
+      },
+    ]
   },
   {
     id: 3,
     title: "Fat Princess",
     cover:
       "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    latestUpdate: "2 new parts",
+    lastRead: "part 8",
+    author: "author name 3",
     views: "1.22M",
     votes: "58.4K",
-    chapters: "46",
     tags: ["gemuk", "romansa", "adult", "+21 more"],
+    chapters: "46",
+    chapterContent: [
+      {
+        id: 1,
+        subtitle: "Chapter 1",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 2,
+        subtitle: "Chapter 2",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 3,
+        subtitle: "Chapter 3",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 4,
+        subtitle: "Chapter 4",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 5,
+        subtitle: "Chapter 5",
+        content: "lorem ipsum....."
+      },
+    ]
   },
   {
     id: 4,
     title: "Book 4",
     cover:
       "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+        latestUpdate: "continue",
+    lastRead: "part 20",
+    author: "author name 5",
     views: "1.33M",
     votes: "14.8K",
-    chapters: "24",
     tags: ["dark", "cinta", "adult", "+15 more"],
+    chapters: "24",
+    chapterContent: [
+      {
+        id: 1,
+        subtitle: "Chapter 1",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 2,
+        subtitle: "Chapter 2",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 3,
+        subtitle: "Chapter 3",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 4,
+        subtitle: "Chapter 4",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 5,
+        subtitle: "Chapter 5",
+        content: "lorem ipsum....."
+      },
+    ]
   },
   {
     id: 5,
     title: "Book 5",
     cover:
       "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    latestUpdate: "continue",
+    lastRead: "part 20",
+    author: "author name 5",
     views: "1.33M",
     votes: "14.8K",
-    chapters: "24",
     tags: ["dark", "cinta", "adult", "+15 more"],
+    chapters: "24",
+    chapterContent: [
+      {
+        id: 1,
+        subtitle: "Chapter 1",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 2,
+        subtitle: "Chapter 2",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 3,
+        subtitle: "Chapter 3",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 4,
+        subtitle: "Chapter 4",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 5,
+        subtitle: "Chapter 5",
+        content: "lorem ipsum....."
+      },
+    ]
   },
 ];
 
