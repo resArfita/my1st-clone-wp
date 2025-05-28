1fa9508d9e315ae3004e0f6cd874ef8dd291f289
res Arfita <hresaarfita@gmail.com>
2025-05-28 09:18:52 +0700

    Bring over the updated files SearchContent.jsx and Books.js from View/Stories branch

---
+++ src/data/Books.js
@@ -0,0 +1,81 @@
+const books = [
+  {
+    id: 1,
+    title: "My Hero, My Boss",
+    cover:
+      "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    views: "1.33M",
+    votes: "14.8K",
+    tags: ["dark", "cinta", "adult", "+15 more"],
+    chapters: "24", //it can be merge with the chapterContent, but can do it later
+    chapterContent: [
+      {
+        id: 1,
+        subtitle: "Chapter 1",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 2,
+        subtitle: "Chapter 2",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 3,
+        subtitle: "Chapter 3",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 4,
+        subtitle: "Chapter 4",
+        content: "lorem ipsum....."
+      },
+      {
+        id: 5,
+        subtitle: "Chapter 5",
+        content: "lorem ipsum....."
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "The Antagonist",
+    cover:
+      "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    views: "7.65M",
+    votes: "408K",
+    chapters: "74",
+    tags: ["miskin", "fiksi", "hidupkembali", "+22 more"],
+  },
+  {
+    id: 3,
+    title: "Fat Princess",
+    cover:
+      "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    views: "1.22M",
+    votes: "58.4K",
+    chapters: "46",
+    tags: ["gemuk", "romansa", "adult", "+21 more"],
+  },
+  {
+    id: 4,
+    title: "Book 4",
+    cover:
+      "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    views: "1.33M",
+    votes: "14.8K",
+    chapters: "24",
+    tags: ["dark", "cinta", "adult", "+15 more"],
+  },
+  {
+    id: 5,
+    title: "Book 5",
+    cover:
+      "https://img.freepik.com/free-photo/spring-blooming_93675-130564.jpg?t=st=1746692379~exp=1746695979~hmac=bffb618b226d41ed0d6492ac5b3f0037196d8d2311b9daf281320f0ae1df7926&w=740",
+    views: "1.33M",
+    votes: "14.8K",
+    chapters: "24",
+    tags: ["dark", "cinta", "adult", "+15 more"],
+  },
+];
+
+export default books;
